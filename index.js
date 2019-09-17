2d182b159208f35aee5401c8d32a10375a3542ff
Sadie Lee <contact@slimekat.com>
2019-09-16 19:59:27 -0700

    Added bootstrap, expanded functionality

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+$(document).ready(function(){
+    $("#copy-btn").click(function(){
+        document.getElementById('color-toast').innerText = document.getElementById('rgb').innerHTML;
+      $(".toast").toast("show");
+    });
+  });
+  
+  var currentColor;
+
 function changeColor(){
     var newColor = makeColor();
     var box = document.getElementById('box').style.backgroundColor = newColor;
     document.getElementById('rgb').innerHTML = newColor;
+    // document.getElementById('color-toast').innerText = newColor;
 }
+
+
 
 function makeColor(){
     var arr = [];
@@ -11,5 +23,24 @@ function makeColor(){
         arr.push(num);
     }
     var newRgb = 'rgb('+arr[0]+','+ arr[1]+','+arr[2]+')';
+    currentColor = newRgb;
     return newRgb;
 }
+
+function copyColor(){
+    // document.getElementById('rgb').select();
+
+    var copyItem = document.querySelector('.rgb');
+    copyItem.focus();
+    copyItem.select();
+
+    try {
+        var success = document.execCommand('copy');
+        var msg = success ? 'successful!' : 'unsuccessful';
+        console.log('Copying text command was.. ' + msg);
+        
+    }catch(err){
+        console.log('Unable to copy');
+    }
+}
+
