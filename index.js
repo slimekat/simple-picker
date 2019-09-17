f1c1aa6da83487f8fd88171ff5e314bf7b795917
Sadie Lee <contact@slimekat.com>
2019-09-17 13:07:41 -0700

    fixed missing copy to clipboard function

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-$(document).ready(function(){
-    $("#copy-btn").click(function(){
+$(document).ready(function () {
+    $("#copy-btn").click(function () {
         document.getElementById('color-toast').innerText = document.getElementById('rgb').innerHTML;
-      $(".toast").toast("show");
-    });
-  });
-  
-  var currentColor;
+        var textArea = document.createElement("textarea");
+        var text = document.getElementById('color-toast').innerHTML;
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        // var copyItem = document.querySelector('.rgb');
+        // copyItem.focus();
+        // copyItem.select();
 
-function changeColor(){
+        try {
+            var success = document.execCommand('copy');
+            var msg = success ? 'successful!' : 'unsuccessful';
+            console.log('Copying text command was.. ' + msg);
+
+        } catch (err) {
+            console.log('Unable to copy');
+        }
+        document.body.removeChild(textArea);
+        $(".toast").toast("show");
+    });
+});
+
+var currentColor;
+
+function changeColor() {
     var newColor = makeColor();
     var box = document.getElementById('box').style.backgroundColor = newColor;
     document.getElementById('rgb').innerHTML = newColor;
@@ -16,31 +35,31 @@ function changeColor(){
 
 
 
-function makeColor(){
+function makeColor() {
     var arr = [];
-    for(var i=0; i < 3; i++){
+    for (var i = 0; i < 3; i++) {
         var num = Math.floor(Math.random() * 256);
         arr.push(num);
     }
-    var newRgb = 'rgb('+arr[0]+','+ arr[1]+','+arr[2]+')';
+    var newRgb = 'rgb(' + arr[0] + ',' + arr[1] + ',' + arr[2] + ')';
     currentColor = newRgb;
     return newRgb;
 }
 
-function copyColor(){
+function copyColor() {
     // document.getElementById('rgb').select();
 
-    var copyItem = document.querySelector('.rgb');
-    copyItem.focus();
-    copyItem.select();
+    // var copyItem = document.querySelector('.rgb');
+    // copyItem.focus();
+    // copyItem.select();
 
-    try {
-        var success = document.execCommand('copy');
-        var msg = success ? 'successful!' : 'unsuccessful';
-        console.log('Copying text command was.. ' + msg);
-        
-    }catch(err){
-        console.log('Unable to copy');
-    }
+    // try {
+    //     var success = document.execCommand('copy');
+    //     var msg = success ? 'successful!' : 'unsuccessful';
+    //     console.log('Copying text command was.. ' + msg);
+
+    // }catch(err){
+    //     console.log('Unable to copy');
+    // }
 }
 
